180e01b2ab711afbfc545f38cbc12c864c7efc28
=Sanahm <=anasmeda95@yahoo.fr>
2017-04-04 20:01:08 +0200

    nouvelle modif sur SendAndSetOutputValue

--- TestSon.java
+++ TestSon.java
@@ -1,17 +1,22 @@
 public class TestSon{
   public static void main(String[] arg){
     GenSineBasic s = new GenSineBasic("gen", 442, 1.0);
-    AudioDataReceiver o = new AudioDataReceiver("output");
-    if(o == null) throw new IllegalArgumentException("nnnnnnnnnnnnn");
-    ModuleAbstract.connect(s.getOutputCommunicationPort(0),o.getInputCommunicationPort(0));
+    AudioDataReceiver output1 = new AudioDataReceiver("output1");
+    AudioDataReceiver output2 = new AudioDataReceiver("output2");
+    if(output1 == null) throw new IllegalArgumentException("nnnnnnnnnnnnn");
+    ModuleAbstract.connect(s.getOutputCommunicationPort(0),output1.getInputCommunicationPort(0));
+    ModuleAbstract.connect(output1.getOutputCommunicationPort(0),output2.getInputCommunicationPort(0));
     for(int i = 0 ; i<5*44100 ; i++){
       s.exec();
-      o.exec();
+      output1.exec();
+      output2.exec();
      // o.displayAudioDataWaveform();
       /**o.saveAudioDataToWavFile("test1.wav");*/
      // o.playAudioData();
     } 
-    o.displayAudioDataWaveform();
-    o.playAudioData();
+    output1.displayAudioDataWaveform();
+    output1.playAudioData();
+    output2.displayAudioDataWaveform();
+    output2.playAudioData();
   }
 }
