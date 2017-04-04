d8c58a082f034eeda9685845a4e48acfb2689223
=Sanahm <=anasmeda95@yahoo.fr>
2017-04-04 18:13:32 +0200

    test

---
+++ TestAudio.java
@@ -0,0 +1,7 @@
+public class TestAudio{
+
+    public static void main(String[] args){
+       GenSineBasic gen = new GenSineBasic(442,1.0);
+       AudioDataReceiver output = new AudioDataReceiver();
+    }
+}
