0e922e1b7a051654591057c63f9b36e89b7802a9
GreenBeaver4223 <baptiste.boudier.phelma@gmail.com>
2017-04-05 14:49:39 +0200

    05/04 14h50

--- GenSine.java
+++ GenSine.java
@@ -5,16 +5,19 @@ public class GenSine extends ModuleAbstract{
 		super(name, 2, 1);
 	}
 
-	@Override
-	public void exec(){
-    double freq = getInputPortValue(0);
+	private double calculEchantillon(){
+		double freq = getInputPortValue(0);
   	double amp = getInputPortValue(1);
 		double e = amp*Math.sin(2*Math.PI*freq*n/44100.0);
-		setAndSendOutputPortValue(0, e);
+	}
+
+	@Override
+	public void exec(){
+		setAndSendOutputPortValue(0, calculEchantillon());
 		n+=1;
 	}
 
-    public void reset(){
-           setOutputPortValue(0,0);
-     }
+  public void reset(){
+    setOutputPortValue(0,0);
+  }
 }
